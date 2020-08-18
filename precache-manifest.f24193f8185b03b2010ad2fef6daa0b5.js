4dad641f63046b178e9aa316827b8845f049601a
Temilollu <temiloluwaodumosu25@gmail.com>
2020-08-18 09:17:42 +0100

    Updates

---
+++ precache-manifest.f24193f8185b03b2010ad2fef6daa0b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38489ee5cde887ff120ea18d4363cf4a",
+    "url": "/rest-countries/index.html"
+  },
+  {
+    "revision": "46b518f353dc671bf4ec",
+    "url": "/rest-countries/static/css/main.21d4c94c.chunk.css"
+  },
+  {
+    "revision": "b26ed732084861ef7ba5",
+    "url": "/rest-countries/static/js/2.5b05a530.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rest-countries/static/js/2.5b05a530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b518f353dc671bf4ec",
+    "url": "/rest-countries/static/js/main.aa07e5f1.chunk.js"
+  },
+  {
+    "revision": "68dab49088ce2f401fb9",
+    "url": "/rest-countries/static/js/runtime-main.656390fb.js"
+  }
+]);
